e9bad25e1fd5369272ae9cb61503ec9077984839
Michael Adams <m-ad@users.noreply.github.com>
2020-03-21 15:36:41 +0100

    Filter by date

--- bikecount-minimal.py
+++ bikecount-minimal.py
@@ -10,7 +10,13 @@ city = st.selectbox(
 	'Select city',
 	('Karlsruhe', 'Freiburg_Wiwilibruecke', 'Freiburg_Hindenburgstr'))
 	
+start = st.date_input('start', value=datetime.date(2020, 1, 1))
+end = st.date_input('end') # no value = today
 
+if (start>end):
+	tmp = start
+	start = end
+	end = tmp
 
 file = 'bikecount_'+city+'_2013-2020.json'
 with open(file,'r') as f:
@@ -26,10 +32,13 @@ df = pd.DataFrame(tmp,columns=['date','bikecount'])
 df['date']=df['date'].astype('datetime64[ns]')
 df['bikecount']=df['bikecount'].astype(float)
 
+#st.write(df.dtypes)
+#st.dataframe(df)
 
+df_filtered = df[( df['date'] > pd.to_datetime(start) ) & ( df['date'] < pd.to_datetime(end) ) ]
 
 fig=plt.figure()
-plt.plot(df['date'],df['bikecount'])
+plt.plot(df_filtered['date'],df_filtered['bikecount'])
 plt.title('Bikecount in '+city.replace('_', ' '))
 plt.xlabel('Datum')
 plt.ylabel('Fahrräder')
